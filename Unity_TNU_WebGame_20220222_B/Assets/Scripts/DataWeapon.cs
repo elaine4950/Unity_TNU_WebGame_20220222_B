1610b7840f76301d21c9efd76f00d89c519c7640
elaine4950 <100185476+elaine4950@users.noreply.github.com>
2022-03-29 16:05:40 +0800

    生成武器

---
+++ Unity_TNU_WebGame_20220222_B/Assets/Scripts/DataWeapon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MengFan
+{
+    /// <summary>
+    /// 武器資料
+    /// 1.飛行速度
+    /// 2.攻擊力
+    /// 3.起始數量
+    /// 4.最大數量
+    /// 5.間隔時間
+    /// 6.生成位置
+    /// </summary>
+    // ScriptableObject 腳本化物件
+    // 作用:將腳本的資料變成物件儲存於Project內(擴充與維護性佳)
+    // CreateAssetMenu 與 ScriptableObject 搭配使用
+    // 作用:在Project 建立此腳本化物件的選單與檔案名稱
+    // meauName 選單名稱、fileName 檔案名稱
+    [CreateAssetMenu(menuName = "MengFan/Data Weapon", fileName = "Data Weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+        [Header("飛行速度"), Range(0, 5000)]
+        public float speed = 500;
+        [Header("攻擊力"), Range(0, 100)]
+        public float attack = 10;
+        [Header("起始數量"), Range(1, 10)]
+        public int countStart = 1;
+        [Header("最大數量"), Range(1, 20)]
+        public int countMax = 3;
+        [Header("間隔時間"), Range(0, 5)]
+        public float interval = 3.5f;
+
+        //資料類型[] 陣列 - 資料結構
+        //作用 : 儲存多筆相同類型的資料
+        [Header("生成位置")]
+        public Vector3[] v35pawnPoint;
+    }
+}
---
+++ Unity_TNU_WebGame_20220222_B/Assets/Scripts/WeaponSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MengFan
+{
+    public class WeaponSystem : MonoBehaviour
+    {
+        [SerializeField, Header("武器資料")]
+        private DataWeapon dataWeapon;
+
+        /// <summary>
+        /// 計時器
+        /// </summary>
+        private float timer;
+        // 繪製圖示事件 ODG
+        // 左用 : 在編輯器內輔助用，執行黨內不會出現
+        private void OnDrawGizmos()
+        {
+            // 1.決定顏色
+            // Color(紅，綠，藍，透明度) - 0 ~ 1
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            // 2. 繪製圖示
+            // 圖示 繪製球體(中心點，半徑)
+            // 取得陣列資料與法 : 陣列資料名稱[索引值]
+
+            // for 迴圈 : 重複處理程式區塊
+            // (初始值 ; 條件 ; 迴圈結束會執行程式)
+            for (int i = 0; i < dataWeapon.v35pawnPoint.Length; i++)
+            {
+                Gizmos.DrawSphere(dataWeapon.v35pawnPoint[i], 0.5f);
+            }
+            
+        }
+
+        private void Update()
+        {
+            SpawnWeapon();
+        }
+        /// <summary>
+        /// 生成武器
+        /// 1.計算時間
+        /// 2.時間累積到間隔時間
+        /// 3.生成武器
+        /// 4.指定在生成位置上
+        /// 5.發射武器
+        /// 6.賦予武器攻擊力
+        /// </summary>
+        private void SpawnWeapon()
+        {
+            // Time.deltaTime 一個影格的時間
+            timer += Time.deltaTime;
+            // print("經過的時間:" + timer);
+
+            // 如果計時器 大於等於 間隔時間 就生成 武器
+            if (timer >= dataWeapon.interval)
+            {
+                print("生成武器");
+                timer = 0;
+            }
+        }
+    }
+}
